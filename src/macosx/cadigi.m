e0c7f6d07b0f7b8a03584ee160e059aa559a5f3b
Nick Sonneveld <nick@sonneveld.name>
2018-04-14 22:08:41 +1000

    osx: refactor cadigi. Adjust for variable write buffer size. Mixer volume fix.

--- src/macosx/cadigi.m
+++ src/macosx/cadigi.m
@@ -32,8 +32,15 @@ static int ca_set_mixer_volume(int);
 
 
 static AUGraph graph;
-static AudioUnit output_unit, converter_unit;
+   
+static AudioUnit converter_unit;
+static AudioUnit mixer_unit;
+static AudioUnit output_unit;
+
 static int audio_buffer_size = 0;
+static unsigned char *audio_buffer = NULL;
+static int audio_buffer_offset = 0;
+
 static char ca_desc[512] = EMPTY_STRING;
 
 
@@ -94,11 +101,40 @@ DIGI_DRIVER digi_core_audio =
 };
 
 
+/*
+A frame is a set of samples for each channel at one point of time
+A packet is a set of frames. PCM just has one frame per packet, but other formats may differ
+*/
 
+// https://developer.apple.com/library/content/documentation/MusicAudio/Conceptual/AudioUnitHostingGuide_iOS/AudioUnitHostingFundamentals/AudioUnitHostingFundamentals.html#//apple_ref/doc/uid/TP40009492-CH3-SW27
 static OSStatus render_callback(void *inRefCon, AudioUnitRenderActionFlags *inActionFlags,
    const AudioTimeStamp *inTimeStamp, UInt32 inBusNumber, UInt32 inNumFrames, AudioBufferList *ioData)
 {
-   _mix_some_samples((unsigned long)ioData->mBuffers[0].mData, 0, TRUE);
+   // printf("render numbuf=%d numframes=%d bufferSize=%d\n", ioData->mNumberBuffers, inNumFrames, ioData->mBuffers[0].mDataByteSize);
+    
+   unsigned char *mData = ioData->mBuffers[0].mData;
+   UInt32 mDataByteRemaining = ioData->mBuffers[0].mDataByteSize;
+   // UInt32 mDataByteRemaining = inNumFrames*2*sizeof(short);
+
+   while (mDataByteRemaining > 0) {
+      if (audio_buffer_offset >= audio_buffer_size) {
+         _mix_some_samples(audio_buffer, 0, TRUE);
+         audio_buffer_offset = 0;
+      }
+
+      UInt32 byteCopyCount = audio_buffer_size - audio_buffer_offset;
+      if (byteCopyCount > mDataByteRemaining) {
+         byteCopyCount = mDataByteRemaining;
+      }
+
+      memcpy(mData, audio_buffer+audio_buffer_offset, byteCopyCount);
+
+      mData += byteCopyCount;
+      mDataByteRemaining -= byteCopyCount;
+      audio_buffer_offset += byteCopyCount;
+
+      // printf("  - transferred %d bytes\n", byteCopyCount);
+   }
    return 0;
 }
 
@@ -119,15 +155,7 @@ static int ca_detect(int input)
 
 static int ca_init(int input, int voices)
 {
-   AudioDeviceID audio_device;
-   ComponentDescription desc;
-   AUNode output_node, converter_node;
-   AURenderCallbackStruct render_cb;
-   UInt32 property_size, buffer_size;
-   AudioStreamBasicDescription input_format_desc, output_format_desc;
-   char device_name[64], manufacturer[64];
-   UInt32 device_name_size, manufacturer_size;
-   char tmp1[256], tmp2[256], tmp3[256];
+   UInt32 property_size;
 
    if (input) {
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Input is not supported"));
@@ -140,33 +168,69 @@ static int ca_init(int input, int voices)
    
    NewAUGraph(&graph);
 
-   desc.componentType = kAudioUnitType_FormatConverter;
-   desc.componentSubType = kAudioUnitSubType_AUConverter;
-   desc.componentManufacturer = kAudioUnitManufacturer_Apple;
-   desc.componentFlags = 0;        
-   desc.componentFlagsMask = 0;   
-   AUGraphNewNode(graph, &desc, 0, NULL, &converter_node);
-   
-   desc.componentType = kAudioUnitType_Output;
-   desc.componentSubType = kAudioUnitSubType_DefaultOutput;
-   desc.componentManufacturer = kAudioUnitManufacturer_Apple;
-   desc.componentFlags = 0;        
-   desc.componentFlagsMask = 0;   
-   AUGraphNewNode(graph, &desc, 0, NULL, &output_node);
-   
-   AUGraphOpen(graph);
-   AUGraphInitialize(graph);
-   
-   AUGraphGetNodeInfo(graph, output_node, NULL, NULL, NULL, &output_unit);
-   AUGraphGetNodeInfo(graph, converter_node, NULL, NULL, NULL, &converter_unit);
+   AUNode converter_node;
+   AudioComponentDescription converter_desc = {0};
+   converter_desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+   converter_desc.componentSubType = kAudioUnitSubType_AUConverter;
+   converter_desc.componentType = kAudioUnitType_FormatConverter;
+   AUGraphAddNode(graph, &converter_desc, &converter_node);
 
+   AUNode mixer_node;
+   AudioComponentDescription mixer_desc = {0};
+   mixer_desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+   mixer_desc.componentSubType = kAudioUnitSubType_StereoMixer;
+   mixer_desc.componentType = kAudioUnitType_Mixer;
+   AUGraphAddNode(graph, &mixer_desc, &mixer_node);
+
+   AUNode output_node;
+   AudioComponentDescription output_desc = {0};
+   output_desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+   output_desc.componentSubType = kAudioUnitSubType_DefaultOutput;
+   output_desc.componentType = kAudioUnitType_Output;
+   AUGraphAddNode(graph, &output_desc, &output_node);
+
+   AUGraphConnectNodeInput(graph, converter_node, 0, mixer_node, 0);
+   AUGraphConnectNodeInput(graph, mixer_node, 0, output_node, 0);
+
+   if (AUGraphOpen(graph)) {
+      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Error opening graph"));
+      ca_exit(FALSE);
+      return -1; 
+   }
+   
+   AUGraphNodeInfo(graph, converter_node, &converter_desc, &converter_unit);
+   AUGraphNodeInfo(graph, mixer_node, &mixer_desc, &mixer_unit);
+   AUGraphNodeInfo(graph, output_node, &output_desc, &output_unit);
+
+   UInt32 buscount = 1;
+   if (AudioUnitSetProperty(mixer_unit, kAudioUnitProperty_ElementCount, kAudioUnitScope_Input, 0, &buscount, sizeof(buscount))) {
+      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Error initialising mixer"));
+      ca_exit(FALSE);
+      return -1; 
+   }
+
+   AURenderCallbackStruct render_cb = {0};
+   render_cb.inputProc = render_callback;
+    if (AUGraphSetNodeInputCallback(graph, converter_node, 0, &render_cb)) {
+      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot set audio rendering callback"));
+      ca_exit(FALSE);
+      return -1;
+   }
+
+
+   AudioStreamBasicDescription output_format_desc = {0};
    property_size = sizeof(output_format_desc);
    if (AudioUnitGetProperty(output_unit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 0, &output_format_desc, &property_size)) {
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot detect output audio format"));
       ca_exit(FALSE);
       return -1;
    }
+   // printf("output sample rate: %f\n", output_format_desc.mSampleRate);
    
+
+   AudioStreamBasicDescription input_format_desc = {0};
+//    input_format_desc.mSampleRate = 22050.0;
+//    input_format_desc.mSampleRate = 44100.0;
    input_format_desc.mSampleRate = output_format_desc.mSampleRate;
    input_format_desc.mFormatID = kAudioFormatLinearPCM;
 #ifdef ALLEGRO_BIG_ENDIAN
@@ -180,26 +244,40 @@ static int ca_init(int input, int voices)
    input_format_desc.mChannelsPerFrame = 2;
    input_format_desc.mBitsPerChannel = 16;
    
-   if (AudioUnitSetProperty(converter_unit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Output, 0, &output_format_desc, sizeof(output_format_desc)) ||
-       AudioUnitSetProperty(converter_unit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &input_format_desc, sizeof(input_format_desc))) {
+   if( AudioUnitSetProperty(converter_unit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &input_format_desc, sizeof(input_format_desc))) {
       ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot configure format converter audio unit"));
       ca_exit(FALSE);
       return -1;
    }
-   
+
    _sound_bits = 16;
    _sound_stereo = TRUE;
-   _sound_freq = (int)output_format_desc.mSampleRate;
-   
-   AUGraphConnectNodeInput(graph, converter_node, 0, output_node, 0);
-   
-   render_cb.inputProc = render_callback;
-   render_cb.inputProcRefCon = NULL;
-   if (AudioUnitSetProperty(converter_unit, kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Input, 0, &render_cb, sizeof(render_cb))) {
-      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot set audio rendering callback"));
+   _sound_freq = (int)input_format_desc.mSampleRate;
+
+
+   // essentially we were guessing what the buffer size would be, but size changes over time if the output
+   // device changes.
+
+   int audio_buffer_size_in_frames = 512;  // Seems to be initally 512 on average
+   int audio_buffer_size_in_samples = audio_buffer_size_in_frames * 2;
+   audio_buffer_size /* in bytes */ = audio_buffer_size_in_samples * sizeof(short);
+   audio_buffer = _AL_MALLOC_ATOMIC(audio_buffer_size);
+   audio_buffer_offset = audio_buffer_size; // set to end so it will fill immediately
+
+   digi_core_audio.voices = voices;
+   if (_mixer_init(audio_buffer_size_in_samples, _sound_freq, _sound_stereo, TRUE, &digi_core_audio.voices)) {
+      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Error initialising mixer"));
       ca_exit(FALSE);
       return -1;
    }
+
+
+   AUGraphInitialize(graph);
+
+   AUGraphStart(graph);
+
+
+   AudioDeviceID audio_device;
 
    property_size = sizeof(audio_device);
    if (AudioUnitGetProperty(output_unit, kAudioOutputUnitProperty_CurrentDevice, kAudioUnitScope_Output, 0, &audio_device, &property_size)) {
@@ -207,32 +285,26 @@ static int ca_init(int input, int voices)
       ca_exit(FALSE);
       return -1;
    }
-   
-   property_size = sizeof(buffer_size);
-   if (AudioDeviceGetProperty(audio_device, 0, false, kAudioDevicePropertyBufferSize, &property_size, &buffer_size)) {
-      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Cannot get CoreAudio device buffer size"));
-      ca_exit(FALSE);
-      return -1;
-   }
-   audio_buffer_size = buffer_size / sizeof(float) * sizeof(short);
-   digi_core_audio.voices = voices;
-   if (_mixer_init(audio_buffer_size / sizeof(short), _sound_freq, _sound_stereo, TRUE, &digi_core_audio.voices)) {
-      ustrzcpy(allegro_error, ALLEGRO_ERROR_SIZE, get_config_text("Error initializing mixer"));
-      ca_exit(FALSE);
-      return -1;
-   }
-   
-   AUGraphStart(graph);
-   
-   device_name_size = sizeof(device_name);
-   manufacturer_size = sizeof(manufacturer);
-   if (!AudioDeviceGetProperty(audio_device, 0, false, kAudioDevicePropertyDeviceName, &device_name_size, device_name) &&
-       !AudioDeviceGetProperty(audio_device, 0, false, kAudioDevicePropertyDeviceManufacturer, &manufacturer_size, manufacturer)) {
+
+   // describe audio device, write to digi_core_audio.desc
+   char device_name[64];
+   char manufacturer[64];
+   UInt32 device_name_size = sizeof(device_name);
+   UInt32 manufacturer_size = sizeof(manufacturer);
+   AudioObjectPropertyAddress deviceNameAddr = {kAudioDevicePropertyDeviceName, kAudioDevicePropertyScopeOutput, 0};
+   AudioObjectPropertyAddress deviceManuAddr = {kAudioDevicePropertyDeviceManufacturer, kAudioDevicePropertyScopeOutput, 0};
+   if (!AudioObjectGetPropertyData(audio_device, &deviceNameAddr, 0, NULL, &device_name_size, device_name) &&
+       !AudioObjectGetPropertyData(audio_device, &deviceManuAddr, 0, NULL, &manufacturer_size, manufacturer)) 
+   {
+      char tmp1[256];
+      char tmp2[256];
+      char tmp3[256];
       uszprintf(ca_desc, sizeof(ca_desc), get_config_text("%s (%s), 16 bits (%d real), %d bps, %s"),
          uconvert_ascii(device_name, tmp1), uconvert_ascii(manufacturer, tmp2), output_format_desc.mBitsPerChannel,
 	 _sound_freq, uconvert_ascii(_sound_stereo ? "stereo" : "mono", tmp3));
    }
    else {
+      char tmp3[256];
       uszprintf(ca_desc, sizeof(ca_desc), get_config_text("16 bits (%d real), %d bps, %s"), output_format_desc.mBitsPerChannel,
 	 _sound_freq, uconvert_ascii(_sound_stereo ? "stereo" : "mono", tmp3));
    }
@@ -252,6 +324,8 @@ static void ca_exit(int input)
    AUGraphClose(graph);
    DisposeAUGraph(graph);
    _mixer_exit();
+   _AL_FREE(audio_buffer);
+   audio_buffer = NULL;
 }
 
 
@@ -266,7 +340,6 @@ static int ca_buffer_size()
 static int ca_set_mixer_volume(int volume)
 {
    float value = (float)volume / 255.0;
-
-   return AudioUnitSetParameter(converter_unit, kAudioUnitParameterUnit_LinearGain, kAudioUnitScope_Output, 0, value, 0);
+   return AudioUnitSetParameter(output_unit, kAudioUnitParameterUnit_LinearGain, kAudioUnitScope_Output, 0, value, 0);
 }
 
