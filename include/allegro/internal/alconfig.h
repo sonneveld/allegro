28c83462d3b6ed2ce6b9437e1ebc0a379b3d13c9
JJS <jjs@jjs.at>
2019-04-12 22:57:53 +1000

    Add iOS support

--- include/allegro/internal/alconfig.h
+++ include/allegro/internal/alconfig.h
@@ -62,6 +62,8 @@
       #include "allegro/platform/alucfg.h"
    #elif defined ALLEGRO_PSP
       #include "allegro/platform/alpspcfg.h"
+   #elif defined ALLEGRO_IOS
+      #include "allegro/platform/alioscfg.h"
    #else
       #error platform not supported
    #endif
--- include/allegro/platform/alplatf.h.cmake
+++ include/allegro/platform/alplatf.h.cmake
@@ -11,6 +11,7 @@
 #cmakedefine ALLEGRO_QNX
 #cmakedefine ALLEGRO_UNIX
 #cmakedefine ALLEGRO_WATCOM
+#cmakedefine ALLEGRO_IOS
 
 /* These are always defined now. */
 #define ALLEGRO_NO_ASM
